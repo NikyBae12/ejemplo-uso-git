a6dd2b30568d4784efd6ac0f0cfdb0fc125e8963
NikyBae12 <yuliysandrita123@gmail.com>
2023-02-17 09:45:03 -0500

    version 1.0 en rama Karol

--- derly-proyecto/src/main.java
+++ derly-proyecto/src/main.java
@@ -5,6 +5,8 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
+        System.out.println("hoajsa");
+        System.out.println("dsfsdf");
 
         Conexion c = new Conexion();
 
